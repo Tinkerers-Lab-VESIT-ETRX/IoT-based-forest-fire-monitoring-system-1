488ff9c3a1c3e85c91692a76161d7d386f0f1754
avanti2108 <87256943+avanti2108@users.noreply.github.com>
2021-07-12 20:08:06 +0530

    Add files via upload

---
+++ firebase python.py
@@ -0,0 +1,46 @@
+import serial
+from firebase import firebase
+from time import sleep
+from datetime import datetime
+import serial.tools.list_ports
+
+
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+
+
+ser = serial.Serial("COM2", 9600)
+print(ser.readline())
+res =1
+i=0
+time=datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+print(time)
+a=[]
+stra=str(ser.readline())
+stra=stra[9:][:-7]
+print(stra)
+
+
+while res:
+     cc=str(1234)
+     print(cc)
+     val=cc
+     firebase1 = firebase.FirebaseApplication('https://forest-fire-monitoring-s-47e19-default-rtdb.firebaseio.com/', None)
+     
+     for i in range(0,4):
+             string1="123"
+             #string1=str(ser.readline())
+             #string1=string1[9:][:-6]
+             data =  { 'date': datetime.now().strftime("%Y-%m-%d"),
+               'reading':stra,
+               'time': datetime.now().strftime("%H:%M")               
+          }
+             result = firebase1.patch('https://forest-fire-monitoring-s-47e19-default-rtdb.firebaseio.com/'+ '/Temperature_data/'+ str(i), data)
+             print(result)
+     
+
+     
+     res=0
+
+
